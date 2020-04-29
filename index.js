abff9a75a212d4338bd03a35c15c9559f205bf26
Brian Salemi <briandsalemi@gmail.com>
2020-04-29 14:50:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ function findMinAndRemoveSorted(array){
 }
 
 function merge(array1, array2){
-  let minFirstHalf = array1[0];
-  let minSecondHalf = array2[0];
   let sorted = [];
 
   while(array1.length != 0 && array2.length != 0){
-    if(minFirstHalf < minSecondHalf){
+    if(array1[0] < array2[0]){
       sorted.push(findMinAndRemoveSorted(array1))
     } else {
       sorted.push(findMinAndRemoveSorted(array2))
